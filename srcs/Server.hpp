827c8d238375efea86485d324131ee90779c3e5b
Marie-aimee RABOURDIN <mrabourd@bess-f2r3s7.clusters.42paris.fr>
2024-01-20 17:27:02 +0100

    trying stuff but not good yet :wrench:

--- srcs/Server.hpp
+++ srcs/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: mrabourd <mrabourd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 14:58:54 by mrabourd          #+#    #+#             */
-/*   Updated: 2024/01/16 16:39:41 by mrabourd         ###   ########.fr       */
+/*   Updated: 2024/01/20 15:24:44 by mrabourd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@
 # include <cstring>
 # include <stdlib.h>
 # include <unistd.h>
+# include <vector>
+# include <fcntl.h>
 
 # define MAX_EVENTS 10
 
@@ -31,7 +33,8 @@ class Server {
 		char *_str_passwd;
 		int _port;
 		std::string _passwd;
-		int _socket_fd;
+		int _server_fd;
+		// char *_msg;
 
 	public:
 		Server();
@@ -43,6 +46,8 @@ class Server {
 
 		void init ();
 		void createEpoll();
+		void recv_and_forward_msg(int fd);
+		std::vector<std::string > split(std::string &s, std::string delimiter);
 
 		ssize_t Send(const char *data, unsigned int len);
 		int Receive(char *buffer, unsigned int len);
