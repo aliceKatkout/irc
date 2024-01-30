cb4959a636645b29577688c9739d207b80af3824
Marie-aimee RABOURDIN <mrabourd@paul-f3Br7s1.clusters.42paris.fr>
2024-01-30 15:57:39 +0100

    small changes in topic :pin:

--- commands/Topic.cpp
+++ commands/Topic.cpp
@@ -6,7 +6,7 @@
 /*   By: mrabourd <mrabourd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:05:34 by mrabourd          #+#    #+#             */
-/*   Updated: 2024/01/29 19:24:35 by mrabourd         ###   ########.fr       */
+/*   Updated: 2024/01/30 15:54:56 by mrabourd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@ void TopicCmd::execute(User *user, std::vector<std::string> args) {
     std::cout << "arg1:" << *(args.begin()) << std::endl;
     std::cout << "arg2:" << *(args.begin() + 1) << std::endl;
 
-    
+    // if the user is the operator of the channel
     user->reply("332 " + *(args.begin()) + " :" + *(args.begin()) );
+
+// When the operator leaves but other clients are in,
+// no one becomes operator and no one can change the topic anymore.
+// The operator is no more an operator when he comes back in 
+// so nobody can change the topic, or any mode, anymore at all.
 
 }
